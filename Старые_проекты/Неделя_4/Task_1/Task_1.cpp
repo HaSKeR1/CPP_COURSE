95b2d60aeed6cddf0833882bf9d160ff5e913831
Иван Горинов <lodimin345@gmail.com>
2024-06-05 20:43:05 +0300

    Сделал первое задание, начал делать второе

--- Старые_проекты/Неделя_4/Task_1/Task_1.cpp
+++ Старые_проекты/Неделя_4/Task_1/Task_1.cpp
@@ -1,20 +1,140 @@
-﻿// Task_1.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
-//
-
-#include <iostream>
+﻿#include <iostream>
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int pizzaChoice, drinkChoice, pizzaAmount = 0, drinkAmount = 0, pizzaPrice, drinkPrice, pizzaAmount2, totalPriceWithoutDiscount;
+    double totalPrice, drinkTotalPrice;
+    setlocale(LC_ALL, "rus");
+
+    // Выбор пиццы
+    std::cout << "\n1 - Пепперони (17$)\n2 - Маргарита (16$)\n3 - Четыре сыра (19$)\n4 - Гавайская (15$)";
+    std::cout << "\nВведите номер пиццы: ";
+    std::cin >> pizzaChoice;
+
+    // Выбор количества пицц
+    switch (pizzaChoice)
+    {
+    case 1:
+        std::cout << "\nВыбрана пицца 'Пепперони'";
+        std::cout << "\nВведите количество пицц: ";
+        std::cin >> pizzaAmount;
+        pizzaPrice = 17;
+        break;
+    case 2:
+        std::cout << "\nВыбрана пицца 'Маргарита'";
+        std::cout << "\nВведите количество пицц: ";
+        std::cin >> pizzaAmount;
+        pizzaPrice = 16;
+        break;
+    case 3:
+        std::cout << "\nВыбрана пицца 'Четыре сыра'";
+        std::cout << "\nВведите количество пицц: ";
+        std::cin >> pizzaAmount;
+        pizzaPrice = 19;
+        break;
+    case 4:
+        std::cout << "\nВыбрана пицца 'Гавайская'";
+        std::cout << "\nВведите количество пицц: ";
+        std::cin >> pizzaAmount;
+        pizzaPrice = 15;
+        break;
+    default:
+        std::cout << "\nОшибка!";
+    }
+
+    // Выбор напитка
+    std::cout << "\n1 - Кола (3$)\n2 - Фанта (2$)\n3 - Сок(1$)";
+    std::cout << "\nВведите номер напитка:";
+    std::cin >> drinkChoice;
+
+    // Выбор количества напитков
+    switch (drinkChoice)
+    {
+    case 1:
+        std::cout << "\nВыбран напиток 'Кола'";
+        std::cout << "\nВведите количество напитков: ";
+        std::cin >> drinkAmount;
+        drinkPrice = 3;
+        break;
+    case 2:
+        std::cout << "\nВыбран напиток 'Фанта'";
+        std::cout << "\nВведите количество напитков: ";
+        std::cin >> drinkAmount;
+        drinkPrice = 2;
+        break;
+    case 3:
+        std::cout << "\nВыбран напиток 'Сок'";
+        std::cout << "\nВведите количество напитков: ";
+        std::cin >> drinkAmount;
+        drinkPrice = 1;
+        break;
+    default:
+        std::cout << "Ошибка!";
+    }
+
+
+    drinkTotalPrice = drinkAmount * drinkPrice;
+    totalPriceWithoutDiscount = pizzaAmount * pizzaPrice + drinkAmount * drinkPrice;
+
+    // Если напиток стоит больше 2$ и их больше 3, то 15% на напитки
+    if (drinkPrice >= 2 and drinkAmount >= 3)
+    {
+        drinkTotalPrice = drinkTotalPrice - drinkTotalPrice * 0.15;
+    }
+
+    // Общая цена 
+    totalPrice = pizzaAmount * pizzaPrice + drinkTotalPrice;
+
+    // Если сумма заказа больше 50$, то -20%
+    if (totalPrice >= 50)
+    {
+        totalPrice = totalPrice - totalPrice * 0.2;
+    }
+
+
+    // Пятая пицца - бесплатно
+    pizzaAmount2 = pizzaAmount;
+    while (pizzaAmount2 > 5)
+    {
+        totalPrice = totalPrice - pizzaPrice;
+        pizzaAmount2 = pizzaAmount2 - 5;
+    }
+
+    // "Чек"
+    std::cout << "\n\n       ЧЕК";
+
+    // Вывод про пиццу
+    switch (pizzaChoice)
+    {
+    case 1:
+        std::cout << "\nПепперони - " << pizzaAmount << "шт - " << pizzaPrice << "$";
+        break;
+    case 2:
+        std::cout << "\nМаргарита - " << pizzaAmount << "шт - " << pizzaPrice << "$";
+        break;
+    case 3:
+        std::cout << "\nЧетыре сыра - " << pizzaAmount << "шт - " << pizzaPrice << "$";
+        break;
+    case 4:
+        std::cout << "\nГавайская - " << pizzaAmount << "шт - " << pizzaPrice << "$";
+        break;
+    }
+
+    //Вывод про напитки
+    switch (drinkChoice)
+    {
+    case 1:
+        std::cout << "\nКола - " << drinkAmount << "шт - " << drinkPrice << "$";
+        break;
+    case 2:
+        std::cout << "\nФанта - " << drinkAmount << "шт - " << drinkPrice << "$";
+        break;
+    case 3:
+        std::cout << "\nСок - " << drinkAmount << "шт - " << drinkPrice << "$";
+        break;
+    }
+
+    // Итого
+    std::cout << "\n\nЦена без скидки: " << totalPriceWithoutDiscount;
+    std::cout << "\nЦена со скидкой: " << totalPrice;
 }
-
-// Запуск программы: CTRL+F5 или меню "Отладка" > "Запуск без отладки"
-// Отладка программы: F5 или меню "Отладка" > "Запустить отладку"
-
-// Советы по началу работы 
-//   1. В окне обозревателя решений можно добавлять файлы и управлять ими.
-//   2. В окне Team Explorer можно подключиться к системе управления версиями.
-//   3. В окне "Выходные данные" можно просматривать выходные данные сборки и другие сообщения.
-//   4. В окне "Список ошибок" можно просматривать ошибки.
-//   5. Последовательно выберите пункты меню "Проект" > "Добавить новый элемент", чтобы создать файлы кода, или "Проект" > "Добавить существующий элемент", чтобы добавить в проект существующие файлы кода.
-//   6. Чтобы снова открыть этот проект позже, выберите пункты меню "Файл" > "Открыть" > "Проект" и выберите SLN-файл.
