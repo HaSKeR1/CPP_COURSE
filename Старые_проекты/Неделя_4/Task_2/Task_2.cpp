a23d894789266f1cac19b6d49e1a149fea668036
Иван Горинов <lodimin345@gmail.com>
2024-06-06 15:35:20 +0300

    Доделал второе задание

--- Старые_проекты/Неделя_4/Task_2/Task_2.cpp
+++ Старые_проекты/Неделя_4/Task_2/Task_2.cpp
@@ -3,57 +3,87 @@
 int main()
 {
 	setlocale(LC_ALL, "rus");
-	int firstSales, secondSales, thirdSales, firstSalary, secondSalary, thirdSalary;
+	int firstSales, secondSales, thirdSales, bestManager;
+	double firstSalary, secondSalary, thirdSalary;
 	
 	// Ввод количества продаж менеджеров
-	std::cout << "Введите количество продаж первого менеджера:";
+	std::cout << "Введите количество продаж первого менеджера: ";
 	std::cin >> firstSales;
-	std::cout << "Введите количество продаж второго менеджера:";
+	std::cout << "Введите количество продаж второго менеджера: ";
 	std::cin >> secondSales;
-	std::cout << "Введите количество продаж третьего менеджера:";
+	std::cout << "Введите количество продаж третьего менеджера: ";
 	std::cin >> thirdSales;
 
 	// Уровень продаж первого менеджера
 	if (firstSales < 500)
 	{
-		firstSalary = firstSales + 0.03 * firstSales;
+		firstSalary = 200 + 0.03 * firstSales;
 	}
 	else if (firstSales >= 500 and firstSales < 1000)
 	{
-		firstSalary = firstSales + 0.05 * firstSales;
+		firstSalary = 200 + 0.05 * firstSales;
 	}
 	else if (firstSales >= 1000)
 	{
-		firstSalary = firstSales + 0.08 * firstSales;
+		firstSalary = 200 + 0.08 * firstSales;
 	}
 
 	// Уровень продаж второго менеджера
 	if (secondSales < 500)
 	{
-		secondSalary = secondSales + 0.03 * secondSales;
+		secondSalary = 200 + 0.03 * secondSales;
 	}
 	else if (secondSales >= 500 and secondSales < 1000)
 	{
-		secondSalary = secondSales + 0.05 * secondSales;
+		secondSalary = 200 + 0.05 * secondSales;
 	}
 	else if (secondSales >= 1000)
 	{
-		secondSalary = secondSales + 0.08 * secondSales;
+		secondSalary = 200 + 0.08 * secondSales;
 	}
 
 	// Уровень продаж третьего менеджера
 	if (thirdSales < 500)
 	{
-		thirdSalary = thirdSales + 0.03 * thirdSales;
+		thirdSalary = 200 + 0.03 * thirdSales;
 	}
 	else if (thirdSales >= 500 and thirdSales < 1000)
 	{
-		thirdSalary = thirdSales + 0.05 * thirdSales;
+		thirdSalary = 200 + 0.05 * thirdSales;
 	}
 	else if (thirdSales >= 1000)
 	{
-		thirdSalary = thirdSales + 0.08 * thirdSales;
+		thirdSalary = 200 + 0.08 * thirdSales;
 	}
 
-	std::cout << secondSalary;
+	// Поиск лучшего менеджера
+	bestManager = firstSalary;
+
+	if (bestManager < secondSalary)
+	{
+		bestManager = secondSalary;
+	}
+	else if (bestManager < thirdSalary)
+	{
+		bestManager = thirdSalary;
+	}
+	
+	// Выдача премии 200$
+	if (bestManager == firstSalary)
+	{
+		firstSalary += 200;
+	}
+	else if (bestManager == secondSalary)
+	{
+		secondSalary += 200;
+	}
+	else if (bestManager == thirdSalary)
+	{
+		thirdSalary += 200;
+	}
+
+	// Вывод итогов
+	std::cout << "\nЗарплата первого менеджера: " << firstSalary << "$\nЗарплата второго менеджера: " << secondSalary << "$\nЗарплата третьего менеджера: " << thirdSalary << "$";
+
+	return 0;
 }
