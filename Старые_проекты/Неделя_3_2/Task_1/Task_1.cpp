ffed0ba7c939ea45ea75401d71b392155b563897
Иван Горинов <lodimin345@gmail.com>
2024-06-25 23:59:14 +0300

    Сделал домашние задания второго задания 3 недели

---
+++ Старые_проекты/Неделя_3_2/Task_1/Task_1.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    int pastHour, pastMinute, pastSecond, hourLeft, minuteLeft, secondLeft, secondsInDay = 86400;
+
+    std::cout << "Введите время в секундах, прошедшее с начала дня: ";
+    std::cin >> pastSecond;
+
+    secondsInDay -= pastSecond;
+
+    pastHour = pastSecond / 60 / 60;
+    pastMinute = pastSecond / 60 - pastHour * 60;
+    pastSecond = pastSecond - (pastHour * 60 * 60 + pastMinute * 60);
+
+    hourLeft = secondsInDay / 60 / 60;
+    minuteLeft = secondsInDay / 60 - hourLeft * 60;
+    secondLeft = secondsInDay - (hourLeft * 60 * 60 + minuteLeft * 60);
+
+    std::cout << "Текущее время: " << pastHour << " часов " << pastMinute << " минут " << pastSecond << " секунд\n";
+    std::cout << "До полуночи осталось: " << hourLeft << " часов " << minuteLeft << " минут " << secondLeft << " секунд";
+
+    return 0;
+}
---
+++ Старые_проекты/Неделя_3_2/Task_2/Task_2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+int main()
+{
+    int pastHour, pastSecond, hourLeft;
+
+    std::cout << "Введите время в секундах, прошедшее с начала рабочего дня:\n";
+    std::cin >> pastSecond;
+
+    pastHour = pastSecond / 60 / 60;
+    hourLeft = 8 - pastHour;
+
+    std::cout << "Осталось работать " << hourLeft << " часов";
+    return 0;
+}
