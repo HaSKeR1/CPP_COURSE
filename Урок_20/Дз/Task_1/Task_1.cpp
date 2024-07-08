a173b3c6b4b9c4b4cf2b42e2b42180bfe881d472
Смирнов Арслан <arslansmirnov38@gmail.com>
2024-07-08 20:57:31 +0300

    Начал дз

---
+++ Урок_20/Дз/Task_1/Task_1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cmath>
+#include <string>
+
+struct complexNumber
+{
+    double real;
+    double img;
+};
+
+void printComplexNumber(complexNumber const& complexNumber)
+{
+    std::cout << std::noshowpos << complexNumber.real;
+    std::cout << std::showpos << complexNumber.img << "i" << std::endl;
+}
+
+//4 функции
+
+complexNumber Sum(complexNumber const& c1, complexNumber const& c2)
+{
+    complexNumber result;
+
+
+
+    return result;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+
+}
