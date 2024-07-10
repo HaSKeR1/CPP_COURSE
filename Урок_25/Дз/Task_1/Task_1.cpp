9de4e8f9c7df91e666fb9249c2eca0452b20e91c
Смирнов Арслан <arslansmirnov38@gmail.com>
2024-07-10 20:50:18 +0300

    Дз

---
+++ Урок_25/Дз/Task_1/Task_1.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+
+    std::ifstream inputFile("example.txt");
+    std::string currentRow;
+    std::vector<std::string> fileRows;
+
+    if (!inputFile.is_open())
+    {
+        std::cout << "Не удалось открыть файл." << std::endl;
+        return 1;
+    }
+
+    while (std::getline(inputFile, currentRow))
+    {
+        fileRows.push_back(currentRow);
+    }
+    std::ofstream outputFile("result.txt");
+
+    for (int i = 0; i < fileRows.size() - 1; i++)
+    {
+        outputFile << fileRows[i] << std::endl;
+    }
+
+    inputFile.close();
+    outputFile.close();
+    return 0;
+}
---
+++ Урок_25/Дз/Task_2/Task_2.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+int main()
+{
+    setlocale(LC_ALL, "rus");
+    std::ifstream inputFile("example.txt");
+    std::string currentRow;
+    std::vector<std::string> fileRows;
+
+    if (!inputFile.is_open())
+    {
+        std::cout << "Не удалось открыть файл." << std::endl;
+        return 1;
+    }
+
+    while (std::getline(inputFile, currentRow))
+    {
+        fileRows.push_back(currentRow);
+    }
+
+    inputFile.close();
+    return 0;
+}
